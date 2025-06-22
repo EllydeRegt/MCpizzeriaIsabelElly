c8dfc8ed05c6ef9c5b1593d01e3d75d0c69005d6
EllydeRegt <114539@montessoricollege.nl>
2025-06-22 15:10:14 +0200

    bord laten zien, streepjes weg

--- po-zeeslagGUI.py
+++ po-zeeslagGUI.py
@@ -5,19 +5,29 @@
 
 import random
 
+spelAfgelopen = False
+aantalPogingen = 0
+
 ### FUNCTIEDEFINITIE ###
 
-def maakBord(aantalKolommen, aantalRijen):
-    bord = []
-    rij = ["-"*aantalKolommen]
-    for y in range(aantalRijen):
-       bord.append(rij)
-    print("leeg bord gemaakt")
-    # bord = [ ["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"] ]
-    return bord
+def maakBord():
+    invoer = input("Hoe breedt wil je het bord hebben?")
+    breedte = int(invoer)                       # zet de input om in een integer
+    print(breedte)
+    invoer = input("Hoe hoog wil je het bord hebben?")
+    hoogte = int(invoer)                        # zet de input om in een integer
+    print(hoogte)
+    rij = ["-"*breedte]                       #maakt een rij met juiste aantal kolommen
+    for y in range(hoogte):
+        bord = rij * hoogte                   #zet deze rij zovaal in het bord als je rijen wilt
+    return bord, breedte, hoogte
 
-def toonBord(lengte, breedte):
-    for rij in range(lengte):
+def toonBord(hoogte, breedte):
+    for kolom in range(breedte):
+        print(" ", chr(65+kolom), end="")       #spatie en dan de letters boven het bord
+    print()                                     #vanaf hier een enter en verder met het bord
+    for rij in range(hoogte):
+        print(1 + rij, end="")                  #de cijfers langs het bord
         for kolom in range(breedte):
             print(bord[rij][kolom], end="")
         print()
@@ -46,8 +56,9 @@ def toonBord(lengte, breedte):
 #                 print(rij3[0:p])
 #     return p
 
-def plaatsSchepen(bord):
-    bord[random.choice(bord)][random.choice(bord)] = "0"
+
+def plaatsSchepen(bord, breedte, hoogte):
+    bord[random.choice(hoogte)][random.choice(breedte)] = "0"
     return bord
 
 # def vraagSpelerOmCoordinaten():
@@ -56,19 +67,44 @@ def plaatsSchepen(bord):
 
 
 ### HOOFDPROGRAMMA ###
-        #maak een leeg bord
-breedte = 3
-lengte = 3
-bord = maakBord(breedte, lengte)
-toonBord(lengte, breedte)
 
-        #vul bord met willekeurige schepen
+#maak een leeg bord
+# invoer = input("Hoe breedt wil je het bord hebben?")
+# breedte = int(invoer)       # zet de input om in een integer
+# print(breedte)
+# invoer = input("Hoe hoog wil je het bord hebben?")
+# hoogte = int(invoer)        # zet de input om in een integer
+# print(hoogte)
+bord, breedte, hoogte = maakBord()
+
+#vul bord met willekeurige schepen
+print(bord)
+# plaatsSchepen(bord, breedte, hoogte)
+#toon bord met schepen op het scherm
+
+#zolang spel niet is afgelopen, doe dan:
+while spelAfgelopen == False:
+    #vraag speler om invoer
+    invoer = input("Voer een coordinaat in")
+    print(invoer)
+    #tel poging
+    #verwerk schot: controleer of raak/mis, vertel gebruiker, pas bord aan
+    #toon bord met schepen op het scherm
+    toonBord(hoogte, breedte)
+
+#spel afgelopen: geef gebruiker een compliment
+if spelAfgelopen == True:
+    print("Goed gespeeld!")
+
+
+
+
+#vul bord met willekeurige schepen
 #plaatsSchepen()
-
-        #toon bord met schepen op het scherm
-        #zolang spel niet is afgelopen, doe dan:
-        #vraag speler om invoer
-        #tel poging
-        #verwerk schot: controleer of raak/mis, vertel gebruiker, pas bord aan
-        #toon bord met schepen op het scherm
-        #spel afgelopen: geef gebruiker een compliment
+#toon bord met schepen op het scherm
+#zolang spel niet is afgelopen, doe dan:
+    #vraag speler om invoer
+    #tel poging
+    #verwerk schot: controleer of raak/mis, vertel gebruiker, pas bord aan
+    #toon bord met schepen op het scherm
+#spel afgelopen: geef gebruiker een compliment
